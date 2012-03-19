4c7081f8d53903f8817e035734c2bf8d4d965cf5
knightsray <knightsray@gmail.com>
2012-03-19 22:57:04 +0900

    Test commit.

---
+++ cmdutil.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python 
+
+import subprocess
+
+class CmdUtil():
+    def __init__(self, cmdpath):
+	self.cmdpath = cmdpath
+    def __del__(self):
+        if self.stdin != None:
+           self.stdin.close()
+        if self.stdout != None:
+           self.stdout.close()
+        if self.stderr != None:
+           self.stderr.close()
+
+    def run(self):
+        self.popen = subprocess.Popen(self.cmdpath,          \
+                                      stdin=subprocess.PIPE, \
+                                      stdout=subprocess.PIPE,\
+                                      stderr=subprocess.PIPE)
+        self.stdin = self.popen.stdin
+        self.stdout = self.popen.stdout
+        self.stderr = self.popen.stderr
+    def poll(self):
+        self.popen.poll()
+    def wait(self):
+        self.popen.wait()
+
+    def printOut(self):
+        for line in self.stdout:
+            print line
+    def printErr(self):
+        for line in self.stderr:
+            print line
+
+    def printStatus(self):
+        print self.popen.returncode
+    def printPid(self):
+        print self.popen.pid
+    def isKilled(self):
+        if self.popen.returncode < 0:
+           return True
+        else:
+           return False
+
+if __name__ == "__main__":
+
+    ls = CmdUtil('ls')
+    ls.run()
+    ls.wait()
+    ls.printOut()
+    ls.printStatus()

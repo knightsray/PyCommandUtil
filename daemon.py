caed6c8018e4875b0351dbbb6c38ffa4a36af662
knightsray <knightsray@gmail.com>
2012-04-07 10:59:31 +0900

    Modify to redirect null device.

--- daemon.py
+++ daemon.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 
-import os, sys
+import os, sys, time
 
 # Simple daemon.
 
 if __name__ == '__main__':
 
+    STDIN = 0
+    STDOUT = 1
+    STDERR = 2
     pid_file_path = './daemon.pid'
 
     # Create a child process.
@@ -17,7 +20,7 @@ if __name__ == '__main__':
     # Create a new session.
     os.setsid()
 
-    # Create a child process.
+    # Create a grand child process.
     pid = os.fork()
 
     if pid > 0:
@@ -34,14 +37,13 @@ if __name__ == '__main__':
     # Set umask.
     os.umask(0)
 
-    # Close stdin, stdout, stderr.
-    sys.stdin.close()
-    sys.stdin = None
-    sys.stdout.close()
-    sys.stdout = None
-    sys.stderr.close()
-    sys.stderr = None
-    os.close(0)
-    os.close(1)
-    os.close(2)
+    # Redirect stdin, stdout, stderr to null device.
+    dev_null = os.open(os.devnull, os.O_RDWR)
+    if dev_null == -1:
+        sys.exit(1)
 
+    os.dup2(dev_null, STDIN)
+    os.dup2(dev_null, STDOUT)
+    os.dup2(dev_null, STDERR)
+
+    time.sleep(60)

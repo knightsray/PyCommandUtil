8cbd9e6ba0325a43e05fb214221cf81094ddce46
knightsray <knightsray@gmail.com>
2012-04-20 05:31:58 +0900

    Add timer

---
+++ timer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+import threading, time
+
+class Timer:
+    def __init__(self, interval, function):
+        self.interval = interval
+        self.handler = function
+        self.keep_timer = True
+        self.status = 0
+
+    def run(self):
+        self.status = 1
+        timer_thread = threading.Timer(self.interval, self.handler)
+        timer_thread.start()
+        timer_thread.join()
+        self.status = 0
+
+    def periodic_run(self):
+        self.keep_timer = True
+        self.status = 1
+        while self.keep_timer:
+            timer_thread = threading.Timer(self.interval, self.handler)
+            timer_thread.start()
+            timer_thread.join()
+        self.status = 0
+
+    def reset_interval(self, interval):
+        if self.status == 0:
+            self.interval = interval
+            return 0
+        else:
+            return -1
+
+    def reset_handler(self, function):
+        if self.status == 0:
+            self.handler = function
+            return 0
+        else:
+            return -1
+
+    def stop_timer(self):
+         if self.status == 1:
+             self.keep_timer = False
+             return 0
+         else:
+             return -1
+
+def func1():
+    print 'fire!!'
+
+if __name__ == '__main__':
+    timer = Timer(1, func1)
+    timer.run()
